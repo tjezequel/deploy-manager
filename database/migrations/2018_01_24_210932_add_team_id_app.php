41a07270f35de54912a5514583d43b52f95d32ad
Thomas JEZEQUEL <jezequel.tho@gmail.com>
2018-01-24 22:12:43 +0100

    Added Team Id on Apps

---
+++ database/migrations/2018_01_24_210932_add_team_id_app.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTeamIdApp extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('apps', function (Blueprint $table) {
+            $table->integer('team_id');
+
+            $table->foreign('team_id')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
